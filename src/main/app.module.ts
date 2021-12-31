b002385c5f368da9f68d29a2f1e26f0a68de2e6e
“Breno <brenorodriguesdev@gmail.com>
2021-12-31 14:41:00 -0300

    refactor: change entrypoint file

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DashboardPage } from 'src/presentation/pages/dashboard/dashboard';
+import { SignInPage } from 'src/presentation/pages/sign-in/sign-in';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInPage,
+    DashboardPage
   ],
   imports: [
     BrowserModule,
---
+++ src/presentation/pages/sign-in/sign-in.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sign-in',
+  templateUrl: './sign-in.html',
+})
+export class SignInPage {
+}
